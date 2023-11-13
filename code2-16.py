edcccf0a2f7ff6f071364b275f8a5ade9d009b98
Shiki0719am <kunuto0719am@gmail.com>
2023-11-13 10:55:24 +0900

    教科書100ページディクショナリ終わり

---
+++ code2-16.py
@@ -0,0 +1,8 @@
+scores = {"network":60, "database":80, "security":50}
+# del scores["security"]
+# score = scores.pop("network")
+# print(scores)
+# print(score)
+print(list(scores.keys()))
+print(dict(scores.items()))
+print(scores.values())
